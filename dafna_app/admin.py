5616cb728076bf4b5482f6b65ba51887a0d302c7
Og'abek <ogabek@pop-os.localdomain>
2023-02-18 09:21:42 +0500

    create models

--- dafna_app/admin.py
+++ dafna_app/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import (
+    Katalog,
+    Prodouct, 
+    Prodouct_type,
+    Love,
+    Cart,
+    Video,
+    Main_contacts,
+    Contact
+)
 
 # Register your models here.
+admin.site.register([
+    Katalog, 
+    Prodouct, 
+    Prodouct_type,
+    Love,
+    Cart,
+    Video,
+    Main_contacts,
+    Contact
+    ])
--- dafna_app/models.py
+++ dafna_app/models.py
@@ -1,3 +1,72 @@
 from django.db import models
 
 # Create your models here.
+
+class Katalog(models.Model):
+    name = models.TextField(max_length=225)
+    discrpition = models.CharField(max_length=225)
+    img_url = models.TextField(max_length=225)
+
+    def __str__(self) -> str:
+        return self.name
+    
+class Prodouct_type(models.Model):
+    name = models.CharField(max_length=225)
+    img_url = models.TextField(max_length=225)
+    katalog = models.ForeignKey(Katalog, models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Prodouct(models.Model):
+    name = models.TextField(max_length=225)
+    discrpition = models.CharField(max_length=225)
+    price = models.IntegerField()
+    color = models.TextField(max_length=25)
+    manufacturer = models.TextField(max_length=25)
+    material = models.TextField(max_length=25)
+    prodouct_type = models.ForeignKey(Prodouct_type, models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.name
+    
+class Love(models.Model):
+    score = models.ForeignKey(Prodouct, models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.score.name
+    
+class Cart(models.Model):
+    name = models.TextField(max_length=225)
+    prodouct = models.ForeignKey(Prodouct, models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.prodouct.name
+    
+class Video(models.Model):
+    name = models.TextField(max_length=225)
+    discrpition = models.CharField(max_length=225)
+    video_url = models.TextField(max_length=225)
+
+    def __str__(self) -> str:
+        return self.name
+    
+class Main_contacts(models.Model):
+    operator = models.TextField(max_length=25)
+    menejer = models.TextField(max_length=25)
+    mebel_menejer = models.TextField(max_length=25)
+    guarantee = models.IntegerField(default=0)
+    email = models.EmailField()
+
+    def __str__(self) -> str:
+        return self.operator
+    
+class Contact(models.Model):
+    branches_name = models.TextField(max_length=225)
+    address = models.TextField(max_length=225)
+    datetime = models.DateTimeField(auto_now=True, blank=True, null=True)
+    menefer = models.TextField(max_length=225)
+    main_contacts = models.ForeignKey(Main_contacts, models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.branches_name
