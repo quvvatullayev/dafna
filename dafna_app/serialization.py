da445a4103343195a3b9ac330dddeced76525541
Og'abek <ogabek@pop-os.localdomain>
2023-02-18 09:36:45 +0500

    craete serialization

---
+++ dafna_app/serialization.py
@@ -0,0 +1,51 @@
+from rest_framework import serializers
+from .models import (
+    Katalog,
+    Prodouct, 
+    Prodouct_type,
+    Love,
+    Cart,
+    Video,
+    Main_contacts,
+    Contact
+)
+
+class KatalogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Katalog
+        fields = '__all__'
+
+class ProdouctSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Prodouct
+        fields = '__all__'
+
+class ProdouctTypeSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Prodouct_type
+        fields = '__all__'
+
+class LoveSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Love
+        fields = '__all__'
+
+class CartSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Cart
+        fields = '__all__'
+
+class VideoSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Video
+        fields = '__all__'
+
+class MainContactsSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Main_contacts
+        fields = '__all__'
+
+class ContactSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Contact
+        fields = '__all__'
