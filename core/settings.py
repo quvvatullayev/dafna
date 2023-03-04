c5c09ef8a3a3b4d5f2a36a88c7892da9b955d14f
ogabek <quvvatullayev@gmail.com>
2023-03-04 20:02:02 +0500

    add cors setting

--- core/settings.py
+++ core/settings.py
@@ -37,11 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "corsheaders",
     'rest_framework',
     'dafna_app'
 ]
 
 MIDDLEWARE = [
+    " corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
