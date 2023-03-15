47f75ae3c8b9f8545dff6dae3ea23e6d0f783794
ogabek <quvvatullayev@gmail.com>
2023-03-15 08:32:31 +0500

    create katalog file veiws

---
+++ dafna_app/views/katalog.py
@@ -0,0 +1,96 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.generics import ListAPIView
+from pprint import pprint
+from dafna_app.models import (
+    Katalog,
+)
+from dafna_app.serializer import(
+    KatalogSerializer,
+)
+
+
+
+class AddKatalog(APIView):
+    def post(self, request:Request):
+        """
+        imput:
+            {
+                "name": str,
+                "discrpition": str,
+                "img_url":str
+            }
+        return: json ->
+            {
+                "id": int,
+                "name":str,
+                "discrpition": str,
+                "img_url":str
+            }
+        """
+        data = request.data
+        katalog = KatalogSerializer(data=data)
+        if katalog.is_valid():
+            katalog.save()
+            return Response(katalog.data)
+        return Response(katalog.errors)
+    
+class UpdeateKatalog(APIView):
+    def post(self, request:Request, id):
+        """
+        imput:
+            {
+                "name":(option) str ,
+                "discrpition":(option) str,
+                "img_url":(option)str
+            }
+        return: json ->
+            {
+                "id": int,
+                "name":str
+                "discrpition": str,
+                "img_url":str
+            }
+        """
+        filter_katalog = Katalog.objects.get(id = id)
+        data = request.data
+        updeate_katalog = filter_katalog
+        updeate_katalog.name = data.get('name', updeate_katalog.name)
+        updeate_katalog.discrpition = data.get('discrpition', updeate_katalog.discrpition)
+        updeate_katalog.img_url = data.get('img_url', updeate_katalog.img_url)
+        updeate_katalog.save()
+        katalog = KatalogSerializer(updeate_katalog)
+        return Response(katalog.data)
+
+class GetKatalog(APIView):
+    def get(self, request:Request):
+        """
+        input:get request
+        return: json->
+        {
+            "katalogs": [
+                {
+                "id": int,
+                "name":str
+                "discrpition": str,
+                "img_url":str
+                }
+            ]
+        }
+        """
+        filter_katalog = Katalog.objects.all()
+        katalog = KatalogSerializer(filter_katalog, many = True)
+        data = {"katalogs":katalog.data}
+        return Response(data)
+    
+class DeleteKatalog(APIView):
+    def get(self, request:Request, id):
+        """
+        input:get request http://127.0.0.1:8000/dafna_app/delete_katalog/<id>/
+        return:json->{"OK delete":"200"}
+        """
+        katalog = Katalog.objects.get(id = id)
+        katalog.delete()
+        return Response({"OK delete":"200"})
