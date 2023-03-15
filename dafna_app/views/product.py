0f7fdf8b9e50a7d64e4cd145140ef40e610484d7
ogabek <quvvatullayev@gmail.com>
2023-03-15 09:00:13 +0500

    create product view all file

--- dafna_app/views/product.py
+++ dafna_app/views/product.py
@@ -6,11 +6,13 @@ from dafna_app.models import (
     Katalog,
     Prodouct, 
     Prodouct_type,
+    Prodouct_img,
 )
 from dafna_app.serializer import(
     KatalogSerializer,
     ProdouctSerializers,
     ProdouctTypeSerializers,
+    ProdouctImgSerializers,
 )
 
 # Create your views here.
@@ -160,3 +162,137 @@ class DeleteProdouct(APIView):
         prodouct = Prodouct.objects.get(id = id)
         prodouct.delete()
         return Response({"OK delete":"200"})
+
+class Sort(APIView):
+    def get(self, request:Request, name):
+        """
+        input:get request
+        return:json->
+        [
+            "sorts":
+            [
+                {
+                    "id": int,
+                    "name": str,
+                    "discrpition": str,
+                    "img_url": str,
+                    "price": int,
+                    "color": str,
+                    "manufacturer": int,
+                    "like":boole,
+                    "carts": boole,
+                    "material": str,
+                    "prodouct_type": int
+                }
+            ]
+        ]
+
+        or:json->
+        {
+            "sort": "None"
+        }
+        """
+        prodouct_filter = Prodouct.objects.filter(name__contains = name)
+        prodouct = ProdouctSerializers(prodouct_filter, many = True)
+        if len(prodouct.data) > 0:
+            data = {
+                "sorts":prodouct.data
+            }
+            return Response(data)
+        return Response({'sort':"None"})
+
+class GetNewProduct(APIView):
+    def get(self, request:Request):
+        """
+        input:get request
+        return:json->
+        {
+            "prodoucts": [
+                {
+                    "id": int,
+                    "name": str,
+                    "discrpition": str
+                    "img_url": str,
+                    "price": int,
+                    "color": str,
+                    "manufacturer": str,
+                    "like":bool
+                    "material": str,
+                    "prodouct_type": int
+                }
+            ]
+        }
+        """
+        data = Prodouct.objects.all()[::-1][:10]
+        prodouct = ProdouctSerializers(data, many = True)
+        data = {'prodoucts':prodouct.data}
+        return Response(data)
+
+class GetRecommendationProdouct(APIView):
+    def get(self, request:Request):
+        """
+        input:get request
+        return:json->
+        {
+            "prodoucts": [
+                {
+                    "id": int,
+                    "name": str,
+                    "discrpition": str
+                    "img_url": str,
+                    "price": int,
+                    "color": str,
+                    "manufacturer": str,
+                    "like":bool
+                    "material": str,
+                    "prodouct_type": int
+                }
+            ]
+        }
+        """
+        data = Prodouct.objects.all()[:10]
+        prodouct = ProdouctSerializers(data, many = True)
+        data = {'prodoucts':prodouct.data}
+        return Response(data)
+    
+class GetProdouctDetail(APIView):
+    def get(self, request:Request, id):
+        """
+        input:get request
+        return:json->
+        {
+            "prodouct": {
+                "id": int,
+                "name": str,
+                "discrpition": str
+                "img_url": str,
+                "imgs": list,
+                "price": int,
+                "color": str,
+                "manufacturer": str,
+                "material": str,
+                "like":bool
+                "prodouct_type": int
+            }
+        }
+        """
+        prodouct_filter = Prodouct.objects.get(id = id)
+        prodouct = ProdouctSerializers(prodouct_filter, many = False)
+        prodouct_img_filter = Prodouct_img.objects.filter(prodouct = id)
+        prodouct_img = ProdouctImgSerializers(prodouct_img_filter, many = True)
+        data = {
+            "prodouct":{
+                "id":prodouct.data['id'],
+                "name":prodouct.data['name'],
+                "discrpition":prodouct.data['discrpition'],
+                "img_url":prodouct.data['img_url'],
+                "imgs":prodouct_img.data,
+                "price":prodouct.data['price'],
+                "color":prodouct.data['color'],
+                "manufacturer":prodouct.data['manufacturer'],
+                "material":prodouct.data['material'],  
+                "like":prodouct.data['like'],
+                "prodouct_type":prodouct.data['prodouct_type'],
+            }
+        }
+        return Response(data)
