6c51b9e316f2404649d3ae34fae64680cc884573
ogabek <quvvatullayev@gmail.com>
2023-04-17 09:31:32 +0500

    add token authentication model peckech

--- dafna_app/views/card.py
+++ dafna_app/views/card.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.request import Request
 from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
+
 from dafna_app.models import (
     Prodouct,
     Cart,
